5c4469342e08ed4fa8b728a7de0e58b6809a0647
da-nn-yy <db7394928@gmail.com>
2024-08-21 17:23:51 +0200

    modified: hedaer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import danLogo from '../assets/DanLogo.svg'
+import { Link } from 'react-scroll'
 
 const Header = () => {
   return (
-    <div className="text-white section">
-      About
-    </div>
+    <header className="flex w-full py-10">
+        <div className="container mx-auto">
+          <div className='flex items-center justify-between '>
+            <Link to="home" smooth={true}><img src={danLogo} alt="Dan Logo" /></Link>
+            <button className='btn'>Hire me</button>
+          </div>
+        </div>
+    </header>
   )
 }
 
