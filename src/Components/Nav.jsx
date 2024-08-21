ad978bf0ee6ef7ebfdc12feac4932fa703718178
da-nn-yy <db7394928@gmail.com>
2024-08-21 14:07:57 +0200

    modified: the navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,16 +4,26 @@ import { Link } from "react-scroll";
 
 const Nav = () =>{
   return (
-    <nav>
-      <div>
-        <Link
-            to="home"
-            activeClass="active"
-            smooth={true}
-            spy={true}
-            className="cursor-pointer w-[60] flex items-center justify-center">
-          <GoHomeFill/>
-        </Link>
+    <nav className="fixed w-full overflow-hidden top-2 lg:top-8">
+      <div className="container mx-auto">
+          <div className="flex h-[96px]:items-center justify-around rounded-full bg-black/20 backdrop-blur-2xl max-w=[460px] mx-auto text-2xl text-white">
+            <Link
+                to="home"
+                activeClass="active"
+                smooth={true}
+                spy={true}
+                className="cursor-pointer w-[60] flex items-center justify-center">
+                  Home
+            </Link>
+            <Link
+                to="about"
+                activeClass="active"
+                smooth={true}
+                spy={true}
+                className="cursor-pointer w-[60] flex items-center justify-center">
+                  About
+            </Link>
+          </div>
       </div>
     </nav>
   )
