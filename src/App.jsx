0a151ddc9651d7d4c751ee71a4e16f9e2c55d42a
da-nn-yy <db7394928@gmail.com>
2024-08-21 17:23:14 +0200

    modified my app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import Nav from "./Components/Nav"
 import About from "./Components/About";
 import Hero from "./Components/Hero";
 import Contact from "./Components/Contact";
+import Skills from "./Components/Skills";
+import Header from "./Components/Header";
 function App() {
 
   return (
     <div>
       <Nav/>
+      <Header/>
       <Hero/>
       <About/>
+      <Skills/>
       <Contact/>
       <div className='h-[4000px]'></div>
     </div>
