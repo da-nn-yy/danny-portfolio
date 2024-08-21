e7b302c1b252d4f6eef05917f402fd0d0a1b5254
da-nn-yy <db7394928@gmail.com>
2024-08-21 17:24:22 +0200

    updated the width of nav bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,15 @@
-import { BsClipboardData,BsBriefcase,BsChatFill } from "react-icons/bs";
+import { BsChatFill } from "react-icons/bs";
 import { BiUser } from "react-icons/bi";
+import { CgMenuLeft } from "react-icons/cg";
 import { HiHome } from "react-icons/hi2";
 import { Link } from "react-scroll";
 
 const Nav = () =>{
   return (
-    <nav className="fixed z-50 w-full overflow-hidden top-2 lg:top-8">
+    <nav className="fixed w-full top-2 lg:top-8">
 
       <div className="container mx-auto">
-          <div className="h-[96px] flex items-center justify-around rounded-full bg-black/20 backdrop-blur-2xl max-w=[460px] mx-auto text-2xl text-white">
+          <div className="h-[96px] flex items-center justify-around rounded-full bg-black/20 backdrop-blur-2xl mx-auto text-2xl max-w-[460px] text-white">
             <Link
                 to="home"
                 activeClass="active"
@@ -27,12 +28,12 @@ const Nav = () =>{
                   <BiUser/>
             </Link>
             <Link
-                to="home"
+                to="skills"
                 activeClass="active"
                 smooth={true}
                 spy={true}
                 className="cursor-pointer w-[60] flex items-center justify-center">
-                  Home
+                  <CgMenuLeft/>
             </Link>
             <Link
                 to="contact"
