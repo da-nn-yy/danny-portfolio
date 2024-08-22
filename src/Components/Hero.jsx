0978aa646f0dd9c219c26ba38530ef3cb13397d0
da-nn-yy <db7394928@gmail.com>
2024-08-22 10:32:17 +0200

    modified my hero page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,38 @@
+import { TypeAnimation } from "react-type-animation";
+import { MdOutlineCloudDownload } from "react-icons/md";
 const Hero = () => {
   return (
-    <div className="text-white section" id="home">
-      
-    </div>
+    <section className="text-white section" id="home">
+      <div>
+        <h1>
+          Daniel Merga
+        </h1>
+      </div>
+      <div>
+        <span>
+          I am
+        </span>
+        <TypeAnimation
+        sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Frontend developer',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Graphics Desinger',
+        1000,
+        'UI/UX Desinger',
+        1000,
+      ]}
+      wrapper="span"
+      speed={30}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity} />
+      </div>
+      <p> I am currently studying Computer Science and Engineering at Adama Science and Technology University(ASTU). </p>
+      <div>
+        <a href="">Contact Me</a>
+        <button className="flex items-center justify-around btn btn-sm hover:bg-line">Download CV <MdOutlineCloudDownload/> </button>
+      </div>
+    </section>
   )
 }
 
