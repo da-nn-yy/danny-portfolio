1483bec8ac8c416d165312dde28d969661a459df
da-nn-yy <db7394928@gmail.com>
2024-08-22 16:20:15 +0200

    modified the stracture of app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import Hero from "./Components/Hero";
 import Contact from "./Components/Contact";
 import Skills from "./Components/Skills";
 import Header from "./Components/Header";
+import danVidBg from './assets/danBgg.mp4'
 function App() {
 
   return (
-    <div>
-      <Nav/>
-      <Header/>
-      <Hero/>
-      <About/>
-      <Skills/>
-      <Contact/>
-      <div className='h-[4000px]'></div>
+    <div className="relative w-full overflow-hidden">
+      <video
+        src={danVidBg}
+        autoPlay
+        loop
+        muted
+        className="fixed left-0 object-cover w-full h-full ftop-0 ixed "
+      />
+      <div className="relative z-10">
+        <Nav />
+        <Header />
+        <Hero />
+        <About />
+        <Skills />
+        <Contact />
+        <div className="h-[4000px]"></div>
+      </div>
     </div>
 
   )
