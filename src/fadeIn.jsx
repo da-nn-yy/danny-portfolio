1bf671998fc545df807be62202e5648cf9d34104
da-nn-yy <db7394928@gmail.com>
2024-08-23 12:07:06 +0200

    modified fadin function

--- src/fadeIn.jsx
+++ src/fadeIn.jsx
@@ -2,13 +2,13 @@ export const fadeIn = (direction = 'up', delay = 0) => {
   return {
     hidden: {
       opacity: 0,
-      y: direction === 'up' ? 30 : -30, // Adjust the direction of the animation
+      y: direction === 'up' ? 90 : -90, // Adjust the direction of the animation
     },
     show: {
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.5,
+        duration: 1.5,
         delay: delay,
         ease: 'easeOut',
       },
