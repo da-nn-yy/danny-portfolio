4be73255aaf91a4d8d75079c388827ad250aa4d7
da-nn-yy <db7394928@gmail.com>
2024-08-23 11:08:43 +0200

    modified the Hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,39 +1,55 @@
 import { TypeAnimation } from "react-type-animation";
-import { MdOutlineCloudDownload } from "react-icons/md";
+import logo from '../assets/DanLogo.svg'
+import { motion } from "framer-motion";
+import { fadeIn } from "../fadeIn";
 const Hero = () => {
   return (
-    <section className="text-white section" id="home">
-      <div>
-        <h1>
-          Daniel Merga
-        </h1>
-      </div>
-      <div>
-        <span>
-          I am
-        </span>
-        <TypeAnimation
-        sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'Frontend developer',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Graphics Desinger',
-        1000,
-        'UI/UX Desinger',
-        1000,
-      ]}
-      wrapper="span"
-      speed={30}
-      style={{ fontSize: '2em', display: 'inline-block' }}
-      repeat={Infinity} />
-      </div>
-      <p> I am currently studying Computer Science and Engineering at Adama Science and Technology University(ASTU). </p>
-      <div>
-        <a href="">Contact Me</a>
-        <button className="btn btn-sm">Download CV </button>
+    <section className="flex items-center text-white section justify-items-center" id="home">
+      <div className="container mx-auto">
+          <di className="flex">
+              <div>
+                  <div>
+                    <motion.h1
+                    variants={fadeIn('up',0.3)}
+                    initial='hidden'
+                     whileInView={'show'}
+                    viewport={{once:false,amount:0.7}}
+                    className="text-[55px] font-bold leading=[0.8] lg:text-[110]">
+                      Daniel <span></span>Merga
+                    </motion.h1>
+                  </div>
+                  <div className="text-[55px] font-bold leading=[0.8] lg:text-[110]">
+                    <span>
+                      I am
+                    </span>
+                    <TypeAnimation
+                    sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    'Frontend developer',
+                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                    'Graphics Desinger',
+                    1000,
+                    'UI/UX Desinger',
+                    1000,
+                  ]}
+                  wrapper="span"
+                  speed={30}
+                  style={{ fontSize: '2em', display: 'inline-block' }}
+                  repeat={Infinity} />
+                  </div>
+                  <p> I am currently studying Computer Science and Engineering at Adama Science and Technology University(ASTU). </p>
+                  <div>
+                    <a href="">Contact Me</a>
+                    <button className="btn btn-sm">Download CV </button>
+                  </div>
+              </div>
+          <div>
+            <img src={logo} alt="" />
+          </div>
+          </di>
       </div>
     </section>
   )
 }
 
-export default Hero; 
+export default Hero;
