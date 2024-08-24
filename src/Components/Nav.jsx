73854bc5b086667b3bb6b5e5848d167334cf2cf9
da-nn-yy <db7394928@gmail.com>
2024-08-24 16:42:45 +0200

    modified the nav bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,46 +3,50 @@ import { BiUser } from "react-icons/bi";
 import { FaAlignLeft } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi2";
 import { Link } from "react-scroll";
+import danLogo from '../assets/DanLogo.svg'
 
 const Nav = () =>{
   return (
     <nav className="fixed w-full top-2 lg:top-8">
 
       <div className="container mx-auto">
-          <div className="h-[96px] flex items-center justify-around rounded-full bg-black/10 backdrop-blur-2xl mx-auto text-2xl max-w-[460px] text-white bord-grad">
-            <Link
-                to="home"
-                activeClass="active"
-                smooth={true}
-                spy={true}
-                offset={-200}
-                className="cursor-pointer w-[60] flex items-center justify-center">
-                  <HiHome/>
-            </Link>
-            <Link
-                to="about"
-                activeClass="active"
-                smooth={true}
-                spy={true}
-                className="cursor-pointer w-[60] flex items-center justify-center">
-                  <BiUser/>
-            </Link>
-            <Link
-                to="skills"
-                activeClass="active"
-                smooth={true}
-                spy={true}
-                className="cursor-pointer w-[60] flex items-center justify-center">
-                  <FaAlignLeft/>
-            </Link>
-            <Link
-                to="contact"
-                activeClass="active"
-                smooth={true}
-                spy={true}
-                className="cursor-pointer w-[60] flex items-center justify-center">
-                  <BsChatFill/>
-            </Link>
+          <div><img src={danLogo} alt="Dan Logo" /></div>
+          <div>
+            <div className="h-[96px] flex items-center justify-around rounded-full bg-black/10 backdrop-blur-2xl mx-auto text-2xl max-w-[460px] text-white bord-grad">
+                <Link
+                    to="home"
+                    activeClass="active"
+                    smooth={true}
+                    spy={true}
+                    offset={-200}
+                    className="cursor-pointer w-[60] flex items-center justify-center">
+                      <HiHome/>
+                </Link>
+                <Link
+                    to="about"
+                    activeClass="active"
+                    smooth={true}
+                    spy={true}
+                    className="cursor-pointer w-[60] flex items-center justify-center">
+                      <BiUser/>
+                </Link>
+                <Link
+                    to="skills"
+                    activeClass="active"
+                    smooth={true}
+                    spy={true}
+                    className="cursor-pointer w-[60] flex items-center justify-center">
+                      <FaAlignLeft/>
+                </Link>
+                <Link
+                    to="contact"
+                    activeClass="active"
+                    smooth={true}
+                    spy={true}
+                    className="cursor-pointer w-[60] flex items-center justify-center">
+                      <BsChatFill/>
+                </Link>
+            </div>
           </div>
       </div>
     </nav>
