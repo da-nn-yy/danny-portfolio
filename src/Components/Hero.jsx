7053bbb2c34d3bd75f4c67d2e2d7da40cfa21621
da-nn-yy <db7394928@gmail.com>
2024-08-24 12:04:34 +0200

    modified and styled my hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,11 +2,13 @@ import { TypeAnimation } from "react-type-animation";
 import logo from '../assets/DanLogo.svg'
 import { motion } from "framer-motion";
 import { fadeIn } from "../fadeIn";
+import { BsGithub, BsInstagram, BsTelegram ,BsLinkedin} from "react-icons/bs";
+
 const Hero = () => {
   return (
     <section className="flex items-center text-white section justify-items-center" id="home">
       <div className="container mx-auto">
-          <div className="flex">
+          <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-10">
               <div className="flex-1 text-center font-secondary lg:text-left">
                     <motion.h1
                     variants={fadeIn('up',0.3)}
@@ -14,33 +16,38 @@ const Hero = () => {
                     whileInView={'show'}
                     viewport={{once:false,amount:0.7}}
                     className="text-[55px] font-bold leading=[0.8] lg:text-[110]">
-                      Daniel <span></span>Merga
+                      Daniel <span >Merga</span>
                     </motion.h1>
                   <div>
-                    <span className="">
+                    <span className="mr-4 font-bold text-[36px] lg:text-[45px]">
                       I am
                     </span>
                     <TypeAnimation
                     sequence={[
                     // Same substring at the start will only be typed out once, initially
-                    ' Frontend developer',
+                    'Frontend developer',
                     1000, // wait 1s before replacing "Mice" with "Hamsters"
-                    ' Graphics Desinger',
+                    'Graphics Desinger',
                     1000,
-                    ' UI/UX Desinger',
+                    'UI/UX Desinger',
                     1000,
                   ]}
                   wrapper="span"
                   speed={30}
-                  style={{ fontSize: '2em', display: 'inline-block' }}
                   repeat={Infinity}
-                  className="text-gradient"
+                  className="inline-block font-bold text-transparent bg-gradient-to-r from-blue-400 to-green-400 bg-clip-text text-[36px] lg:text-[45px]"
                   />
                   </div>
                   <p> I am currently studying Computer Science and Engineering at Adama Science and Technology University(ASTU). </p>
-                  <div>
+                  <div className="flex items-center justify-between max-w-[250px] pt-3 pb-3">
                     <a href="">Contact Me</a>
                     <button className="btn btn-sm">Download CV </button>
+                  </div>
+                  <div className="text-[22px] flex justify-between lg:items-center max-w-[150px]">
+                    <a href="https://github.com/da-nn-yy.com" className="link"><BsGithub/></a>
+                    <a href=""  className="link"><BsLinkedin/></a>
+                    <a href="" className="link"><BsTelegram/></a>
+                    <a href="" className="link"><BsInstagram/></a>
                   </div>
               </div>
           <div>
