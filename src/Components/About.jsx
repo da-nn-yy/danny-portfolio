dc1efbfb70521e12be5a0a07ad842a6a5c351346
da-nn-yy <db7394928@gmail.com>
2024-08-27 11:40:43 +0200

    modified my about page with gsap

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,28 +1,50 @@
-import { MdEmail, MdPerson } from 'react-icons/md'
-import danLogo from '../assets/DanLogo.svg'
-import { IoLocationSharp } from 'react-icons/io5'
-import { PiCityFill } from 'react-icons/pi'
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { MdPerson, MdEmail } from 'react-icons/md';
+import { IoLocationSharp } from 'react-icons/io5';
+import { PiCityFill } from 'react-icons/pi';
+import danLogo from '../assets/DanLogo.svg';
+
+gsap.registerPlugin(ScrollTrigger);
+
 const About = () => {
+  const boxRef = useRef(null);
+
+  useEffect(() => {
+    if (boxRef.current) {
+      gsap.to(boxRef.current, {
+        scrollTrigger: {
+          trigger: boxRef.current,
+          start: "top center",
+          scrub: true,
+          end: "+=300",
+        },
+        x: -50,
+      });
+    }
+  }, []);
+
   return (
     <section>
       <div className='container mx-auto'>
         <div className="flex items-center justify-between text-white">
           <img src={danLogo} alt="Dan Logo" />
-          <div>
-            <h1 className=''>About</h1>
-            <p>I love working on Fromtend Designs, Backend Applications,and solving problems.</p>
-            <h4>Personal Info</h4>
+          <div ref={boxRef}>
+            <h1 className='font-secondary'>About</h1>
+            <p>I love working on Frontend Designs, Backend Applications, and solving problems.</p>
+            <h4 className='text-[18px] font-medium'>Personal Info:</h4>
             <ul className='ml-6'>
-              <li className='flex items-center'><MdPerson/>Name: Daniel Merga</li>
-              <li className='flex items-center'><MdEmail/>Email: dbevangadi@gmail.com</li>
-              <li className='flex items-center'><IoLocationSharp/>Residence: Ethiopia</li>
-              <li className='flex items-center'><PiCityFill/>City: Adama</li>
+              <li className='flex items-center'><MdPerson /> Name: Daniel Merga</li>
+              <li className='flex items-center'><MdEmail /> Email: dbevangadi@gmail.com</li>
+              <li className='flex items-center'><IoLocationSharp /> Residence: Ethiopia</li>
+              <li className='flex items-center'><PiCityFill /> City: Adama</li>
             </ul>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
